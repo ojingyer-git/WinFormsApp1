40e5f6ef731cf412c9e1a393d9442583af58bf2c
ke <ke@DESKTOP-OHF77HE>
2025-07-19 11:38:46 +0900

    .

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.jetProgressBar1 = new WinFormsApp1.JetProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(289, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // jetProgressBar1
+            // 
+            this.jetProgressBar1.Location = new System.Drawing.Point(13, 43);
+            this.jetProgressBar1.Name = "jetProgressBar1";
+            this.jetProgressBar1.Size = new System.Drawing.Size(289, 23);
+            this.jetProgressBar1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 29);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.jetProgressBar1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private JetProgressBar jetProgressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WinFormsApp1/JetProgressBar.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WinFormsApp1
+{
+    public class JetProgressBar : ProgressBar
+    {
+        public JetProgressBar()
+        {
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // ProgressBar의 기본 스타일을 사용하지 않도록 설정합니다.
+            e.Graphics.FillRectangle(Brushes.White, e.ClipRectangle);
+
+            // 현재 진행 상태에 따라 색상을 계산합니다.
+            float percent = (float)this.Value / this.Maximum;
+            Color color = GetJetColor(percent);
+
+            // ProgressBar의 진행 부분을 그립니다.
+            Rectangle rect = new Rectangle(0, 0, (int)(this.Width * percent), this.Height);
+            using (SolidBrush brush = new SolidBrush(color))
+            {
+                e.Graphics.FillRectangle(brush, rect);
+            }
+
+            // 테두리를 그립니다.
+            e.Graphics.DrawRectangle(Pens.Black, 0, 0, this.Width - 1, this.Height - 1);
+        }
+
+        // Jet 컬러 스펙트럼을 기반으로 색상을 계산하는 함수입니다.
+        private Color GetJetColor(float value)
+        {
+            // 입력 값을 0에서 1 사이로 정규화합니다.
+            value = Math.Max(0, Math.Min(1, value));
+
+            // Jet 컬러 스펙트럼을 정의합니다.
+            float r, g, b;
+            if (value < 0.25)
+            {
+                r = 0;
+                g = value * 4;
+                b = 1;
+            }
+            else if (value < 0.5)
+            {
+                r = 0;
+                g = 1;
+                b = (0.5f - value) * 4;
+            }
+            else if (value < 0.75)
+            {
+                r = (value - 0.5f) * 4;
+                g = 1;
+                b = 0;
+            }
+            else
+            {
+                r = 1;
+                g = (1 - value) * 4;
+                b = 0;
+            }
+
+            // 색상을 반환합니다.
+            return Color.FromArgb((int)(r * 255), (int)(g * 255), (int)(b * 255));
+        }
+    }
+}
