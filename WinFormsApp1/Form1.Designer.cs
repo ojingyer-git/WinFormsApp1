7a51f16e8666263098b308ed7ab376931e63e2e6
ke <ke@DESKTOP-OHF77HE>
2025-07-19 11:57:09 +0900

    ..

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -29,33 +29,26 @@ namespace WinFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.jetProgressBar1 = new WinFormsApp1.JetProgressBar();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(13, 13);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(289, 23);
-            this.progressBar1.TabIndex = 0;
-            // 
             // jetProgressBar1
             // 
-            this.jetProgressBar1.Location = new System.Drawing.Point(13, 43);
+            this.jetProgressBar1.Location = new System.Drawing.Point(13, 12);
             this.jetProgressBar1.Name = "jetProgressBar1";
             this.jetProgressBar1.Size = new System.Drawing.Size(289, 23);
             this.jetProgressBar1.TabIndex = 1;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(13, 73);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(289, 29);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // Form1
             // 
@@ -64,7 +57,6 @@ namespace WinFormsApp1
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.jetProgressBar1);
-            this.Controls.Add(this.progressBar1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -73,8 +65,6 @@ namespace WinFormsApp1
         }
 
         #endregion
-
-        private System.Windows.Forms.ProgressBar progressBar1;
         private JetProgressBar jetProgressBar1;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -21,5 +21,65 @@ namespace WinFormsApp1
             jetProgressBar1.Value = 20;
         }
 
+        private Color GetJetColor(float value)
+        {
+            // 입력 값을 0에서 1 사이로 정규화합니다.
+            value = Math.Max(0, Math.Min(1, value));
+
+            // Jet 컬러 스펙트럼을 정의합니다.
+            float r, g, b;
+            if (value < 0.25)
+            {
+                r = 0;
+                g = value * 4;
+                b = 1;
+            }
+            else if (value < 0.5)
+            {
+                r = 0;
+                g = 1;
+                b = (0.5f - value) * 4;
+            }
+            else if (value < 0.75)
+            {
+                r = (value - 0.5f) * 4;
+                g = 1;
+                b = 0;
+            }
+            else
+            {
+                r = 1;
+                g = (1 - value) * 4;
+                b = 0;
+            }
+
+            // 색상을 반환합니다.
+            return Color.FromArgb((int)(r * 255), (int)(g * 255), (int)(b * 255));
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+            Graphics g = e.Graphics;
+
+            int nSteps = 24;
+
+            float fStep;
+            fStep = (float)pb.Width / nSteps;
+
+            Rectangle rectFill = new Rectangle();
+            for (int iOnBand = 0; iOnBand < nSteps; iOnBand++)
+            {
+                rectFill.X = iOnBand * (int)fStep;
+                rectFill.Y = 0;
+                rectFill.Width = (int)fStep;
+                rectFill.Height = pb.Height;
+
+                float percent = (float)iOnBand / nSteps;
+                Color clr = GetJetColor(percent);
+
+                g.FillRectangle(new SolidBrush(clr), rectFill);
+            }
+        }
     }
 }
